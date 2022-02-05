f42da524daade4270e894c0cd0c819e148be4eca
LeoAntunesBrombilla <antunes.b.leonardo@gmail.com>
2022-02-05 16:46:08 -0300

    non-constructible-change

---
+++ non-constructible-change.js
@@ -0,0 +1,14 @@
+function nonConstructibleChange(coins) {
+  coins.sort((a, b) => a - b);
+  let value = 1 // minimun possible change to be created;
+
+  for (const coin of coins) {
+     if (coin > value) return value;
+
+    value += coin;
+  }
+
+  return value;
+}
+
+console.log(nonConstructibleChange([5, 7, 1, 1, 2, 3, 22]));
